31a96774e834264c00009612ec672c8fe9dd8cf4
Abhishek-Dige <abhishekdige22@gmail.com>
2025-12-11 02:40:23 +0530

    refined things

--- src/Components/Footer.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-export const Footer = () => {
-  return (
-    <footer class="bg-dark text-white text-center py-3">
-    <p>© 2025 Event Dashboard | Designed by Team Spiders</p>
-  </footer>
-  )
-}
--- src/Pages/contactus.jsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import "./styles/contactus-styles/contactus_style.css"
-
-function ContactUs(){
-    // return(
-        
-    // )
-}
